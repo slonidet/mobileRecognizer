bcb64028f5c0dd6acabf249fd70d6e89d93e071f
dmitry.sazhnev <dmitry.sazhnev@jetruby.com>
2019-09-05 09:52:06 +0300

    Create component with camera snap and sending photo to server

---
+++ App.js
@@ -0,0 +1,101 @@
+'use strict';
+import React, { PureComponent } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import Axios from 'axios';
+
+
+export default class ExampleApp extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pic_path: undefined,
+    };
+  }
+
+  componentDidUpdate() {
+    console.log('staaaaaaaaaaaaart')
+    console.log(this.state.pic_path);
+    var data = new FormData();
+    data.append('my_photo', {
+      uri: this.state.pic_path,
+      name: 'my_photo.jpg',
+      type: 'image/jpg'
+    })
+    fetch('http://192.168.0.11:5000', {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'multipart/form-data'
+      },
+      method: 'POST',
+      body: data
+    });
+    // Axios.post('http://192.168.0.11:5000', data, {
+    //   headers: {
+    //     'Content-Type': 'multipart/form-data'
+    //   }
+    // })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={{ fontSize: 16 }}> {this.state.pic_path} </Text>
+
+      </View>
+    );
+  }
+
+  takePicture = async() => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      this.setState({
+        pic_path: data.uri
+      });
+    }
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+
+AppRegistry.registerComponent('App', () => ExampleApp);
